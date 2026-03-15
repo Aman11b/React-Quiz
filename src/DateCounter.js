12cfc8cdc818effc69e055566a4c201e66369b43
Aman11b <amansingh1100b@gmail.com>
2026-03-15 09:24:02 +0530

    managing related piece of state

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -1,18 +1,37 @@
-import { act, useReducer, useState } from "react";
+import { useReducer } from "react";
 
+const initialState = { count: 0, step: 1 };
 function reducer(curState, action) {
-  console.log(curState, action);
   // return curState + action;
-  if (action.type === "inc") return curState + 1;
-  if (action.type === "dec") return curState - 1;
-  if (action.type === "setCount") return action.payload;
+  // if (action.type === "inc") return curState + 1;
+  // if (action.type === "dec") return curState - 1;
+  // if (action.type === "setCount") return action.payload;
+
+  // return { count: 0, step: 1 };
+  console.log(curState, action);
+  switch (action.type) {
+    case "dec":
+      return { ...curState, count: curState.count - curState.step };
+    case "inc":
+      return { ...curState, count: curState.count + curState.step };
+    case "setCount":
+      return { ...curState, count: action.payload };
+    case "setStep":
+      return { ...curState, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("unknown action");
+  }
 }
 
 function DateCounter() {
   // const [count, setCount] = useState(0);
 
-  const [count, dispatch] = useReducer(reducer, 0);
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   // This mutates the date object.
   const date = new Date("june 21 2027");
@@ -36,12 +55,14 @@ function DateCounter() {
   };
 
   const defineStep = function (e) {
-    setStep(Number(e.target.value));
+    // setStep(Number(e.target.value));
+    dispatch({ type: "setStep", payload: Number(e.target.value) });
   };
 
   const reset = function () {
     // setCount(0);
-    setStep(1);
+    // setStep(1);
+    dispatch({ type: "reset" });
   };
 
   return (
