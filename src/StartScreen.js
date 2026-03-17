df49a961b05183672daa982445e6c38d328d4a3a
Aman11b <amansingh1100b@gmail.com>
2026-03-17 10:07:29 +0530

    handeling loading error and ready status

---
+++ src/StartScreen.js
@@ -0,0 +1,10 @@
+function StartScreen({ numQuestions }) {
+  return (
+    <div className="start">
+      <h2>Welcome to the React Quiz</h2>
+      <h3>{numQuestions} question to test your React Mastry</h3>
+      <button className="btn btn-ui">Let's Start</button>
+    </div>
+  );
+}
+export default StartScreen;
