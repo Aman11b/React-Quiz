6ce621e52000a3c8c8eb780f8c377042eaeae5e2
Aman11b <amansingh1100b@gmail.com>
2026-03-17 08:57:59 +0530

    react quiz

--- src/DateCounter.js
+++ src/DateCounter.js
@@ -99,3 +99,48 @@ export default DateCounter;
  * -> more advance and complex way to manager state
  * - useReducer hook works wth reducer function(pure function),that takes previous state so-called action as an argument and then return the next state
  */
+
+/**
+ * why userReducer
+ * -> state management with useState is not enough in certain situations:
+ * - when component have lot of state variables and state updates,spread across many event handlers all over the component
+ * - multiple state update need to happen at the same time (as a reaction to same event like starting a game)
+ * - updating one peice of state depends on one or multiple other peices of state
+ */
+
+/**
+ * Managing state with useReducer
+ * -> an alternative way of setting state,ideal for complex state and related peice of state
+ * ```
+ * const [state,dispatch]=useReducer(reducer,initialState);
+ * ```
+ * - store related piece of state in a state object
+ * - userReducer needs reducer:function containing all logic to update state,Decouples state logic from component
+ * - reducer: pure function(no side effects) that takes current state and actionb,and return the next state
+ * ```
+ * function reducer(curState, action) {
+  switch (action.type) {
+    case "dec":
+      return { ...curState, count: curState.count - curState.step };
+    case "inc":
+      return { ...curState, count: curState.count + curState.step };
+    case "setCount":
+      return { ...curState, count: action.payload };
+    case "setStep":
+      return { ...curState, step: action.payload };
+    case "reset":
+      return initialState;
+    default:
+      throw new Error("unknown action");
+  }
+}
+ * ```
+- action:object that describe how to update state(has action type and payload)
+- dispatch:function to trigger state updates,by "sending"action from event handlers to the reducer(similar to setState)
+ */
+
+/**
+ * how reducer update state
+ * -> updating state in a component-> dispatch(action -type:"updateDay" payload=23)->reducer(tate action and current state and return)-> next state-> re-render
+ > reducer is called reducer coz its just like arr.reduce(),reduces acuumulate("reduce") action over time
+ */
